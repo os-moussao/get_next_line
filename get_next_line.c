0c9500930da81051cbd85a4f270102ab4371b0a5
os-moussao <78625018+os-moussao@users.noreply.github.com>
2021-11-07 21:50:24 +0100

    spliting logic

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: omoussao <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/06 15:59:45 by omoussao          #+#    #+#             */
-/*   Updated: 2021/11/07 19:36:06 by omoussao         ###   ########.fr       */
+/*   Updated: 2021/11/07 21:48:35 by omoussao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,55 +26,55 @@ void	get_line(char **line, char *buffer, size_t last)
 		*line = ft_strnjoin("", buffer, last);
 }
 
+int	handle_filled_save(char **line, char **save)
+{
+	size_t	index;
+	char	*tmp;
+
+	index = get_eol(*save);
+	if ((*save)[index] == '\n')
+	{
+		*line = ft_strnjoin("", *save, index + 1);
+		if ((*save)[index + 1])
+		{
+			tmp = ft_strnjoin("", *save + index + 1, BUFFER_SIZE + 1);
+			free(*save);
+			*save = tmp;
+		}
+		else
+		{
+			free(*save);
+			*save = NULL;
+		}
+		return (1);
+	}
+	else
+	{
+		*line = *save;
+		*save = NULL;
+		return (0);
+	}
+}
+
 char	*get_next_line(int fd)
 {
 	static char	*save;
 	char		*line;
 	char		*buffer;
-	char		*tmp;
 	ssize_t		last;
 	size_t		index;
 
 	line = NULL;
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
+	if (save && *save)
+		if (handle_filled_save(&line, &save))
+			return (line);
+
+
+
 	buffer = malloc(BUFFER_SIZE + 1);
 	buffer[BUFFER_SIZE] = 0;
-
-	// don't check first char untill its not null
-	if (save && *save)
-	{
-		index = get_eol(save);
-		if (save[index] == '\n')
-		{
-			line = ft_strnjoin("", save, index + 1);
-			
-			// update save
-			if (save[index + 1])
-			{
-				tmp = ft_strnjoin("", save + index + 1, BUFFER_SIZE + 1);
-				free(save);
-				save = tmp;
-			}
-			else
-			{
-				free(save);
-				save = NULL;
-			}
-
-			free(buffer);
-			return (line);
-		}
-		else
-		{
-			// line = ft_strnjoin("", save, BUFFER_SIZE + 1); // like strdup
-			// free(save); // could be done inside strnjoin (pba)
-			// save = NULL;
-			line = save;
-			save = NULL;
-		}
-	}
-	// else if save is empthy or is does not hold a newline
 	last = read(fd, buffer, BUFFER_SIZE);
 	if (last < 0)
 	{
@@ -88,7 +88,6 @@ char	*get_next_line(int fd)
 		if (buffer[index] == '\n')
 		{
 			get_line(&line, buffer, index + 1);
-
 			if (buffer[index + 1])
 				save = ft_strnjoin("", buffer + index + 1, BUFFER_SIZE);
 			else if (save)
@@ -96,7 +95,6 @@ char	*get_next_line(int fd)
 				free(save);
 				save = NULL;
 			}
-
 			free(buffer);
 			return (line);
 		}
