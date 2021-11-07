3f237501a7e971a0ef7f604c17fcde21714740f7
os-moussao <78625018+os-moussao@users.noreply.github.com>
2021-11-07 16:24:35 +0100

    making progress

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,13 @@
 /*   By: omoussao <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/06 15:59:45 by omoussao          #+#    #+#             */
-/*   Updated: 2021/11/07 12:39:39 by omoussao         ###   ########.fr       */
+/*   Updated: 2021/11/07 16:15:07 by omoussao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*get_line(char **line, char *buffer, size_t last)
+void	get_line(char **line, char *buffer, size_t last)
 {
 	char	*tmp;
 
@@ -20,10 +20,10 @@ char	*get_line(char **line, char *buffer, size_t last)
 	{
 		tmp = ft_strnjoin(*line, buffer, last);
 		free(*line);
-		return (tmp);
+		*line = tmp;
 	}
 	else
-		return (ft_strnjoin("", buffer, last));
+		*line = ft_strnjoin("", buffer, last);
 }
 
 char	*get_next_line(int fd)
@@ -32,7 +32,7 @@ char	*get_next_line(int fd)
 	char		*line;
 	char		*buffer;
 	char		*tmp;
-	size_t		last;
+	ssize_t		last;
 	size_t		index;
 
 	line = NULL;
@@ -54,37 +54,44 @@ char	*get_next_line(int fd)
 			free(save);
 			save = tmp;
 
+			free(buffer);
 			return (line);
 		}
 		else
 		{
-			line = ft_strnjoin("", save, BUFFER_SIZE + 1); // like strdup
-			free(save); // could be done inside strnjoin (pba)
+			// line = ft_strnjoin("", save, BUFFER_SIZE + 1); // like strdup
+			// free(save); // could be done inside strnjoin (pba)
+			// save = NULL;
+			line = save;
 			save = NULL;
-			// line = save
-			// save = NULL
 		}
 	}
 	// else if save is empthy or is does not hold a newline
-
-	while ((last = read(fd, buffer, BUFFER_SIZE)))
+	last = read(fd, buffer, BUFFER_SIZE);
+	if (last < 0)
+	{
+		free(buffer);
+		return (NULL);
+	}
+	while (last > 0)
 	{
 		buffer[last] = 0;
 		index = get_eol(buffer);
 		if (buffer[index] == '\n')
 		{
-			line = get_line(&line, buffer, index + 1);
+			get_line(&line, buffer, index + 1);
 
-			// update save
-			if (save)
-				free(save);
 			save = ft_strnjoin("", buffer + index + 1, BUFFER_SIZE);
 
+			free(buffer);
 			return (line);
 		}
 		else
-			line = get_line(&line, buffer, BUFFER_SIZE + 1);
+			get_line(&line, buffer, BUFFER_SIZE + 1);
+		last = read(fd, buffer, BUFFER_SIZE);
 	}
+	if (save)
+		free(save);
 	free(buffer);
 	return (line);
 }
